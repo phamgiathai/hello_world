1313e2bd9d3584822ef08270d41ff57f02f05cb3
phamgiathai <phamgiathai0411@gmail.com>
2025-01-12 16:23:22 +0700

    add greeting

--- greeting.py
+++ greeting.py
@@ -1,2 +1,16 @@
-name = input("Enter your name:")
-print("Hello", name)
+print("Hello! What is your name?")
+name = input("My name is:")
+print('Where are you from?')
+country = input('I am from:')
+if country == 'Germany':
+    print('Es freut mich sehr, dich kennenzulernen',name)
+    print("Ciao!")
+elif country == 'France':
+    print('Enchanté de faire votre connaissance',name)
+    print("Au revoir!")
+elif country == 'Italy':
+    print('È un piacere conoscerti',name)
+    print("Arrivederci!")
+else:
+    print('Nice to meet you',name)
+    print("Goodbye!")
